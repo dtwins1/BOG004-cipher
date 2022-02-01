30c8135d759322768bca87220542608330a45726
Daniela Marin <gopalipa@MacBook-Air-de-Daniela.local>
2022-02-01 10:14:24 -0500

    codido del cifrado cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
 const cipher = {
-  // ...
+
+  encode: function( shiftInput, claveMayuscula){
+    let resultado = "";
+    shiftInput=(shiftInput % 26 + 26) % 26;
+    for (let i=0; i < claveMayuscula.length; i++) {
+      let formula = (claveMayuscula.charCodeAt(i)-65+shiftInput)%26+65;
+      resultado += String.fromCharCode(formula)
+    }
+      //console.log(resultado)
+      return resultado;
+     
+  },
+  decode: function(shiftInput, claveMayuscula){
+    let resultado = "";
+    shiftInput=(shiftInput % 26 - 26) % 26;
+    for (let i=0; i <claveMayuscula.length; i++) { 
+      let formula = (claveMayuscula.charCodeAt(i)-65-shiftInput)%26+65;
+      resultado += String.fromCharCode(formula)
+    }
+    //console.log(resultado)
+    return resultado;
+  },
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+//const ABC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+//cipher.encode(ABC)
+
+document.getElementById("btnCifrar").addEventListener("click", ()=>{
+    const clave= document.getElementById("texto").value
+    const claveMayuscula= clave.toUpperCase()
+    const shiftInput= document.getElementById("shiftInput").value
+    const clavecifrada= cipher.encode(shiftInput, claveMayuscula)
+    document.getElementById("resultado").innerText= `su clave es:${clavecifrada}`;
+    //console.log(claveMayuscula + "\n"  + shiftInput)
+});
+
+//cipher.decode(ABC)
+
+document.getElementById("btnDescifrar").addEventListener("click", ()=>{
+    const clave= document.getElementById("texto").value
+    const claveMayuscula= clave.toUpperCase()
+    const shiftInput= document.getElementById("shiftInput").value
+    const clavedescifrada= cipher.decode(shiftInput, claveMayuscula)
+    document.getElementById("resultado").innerText= `su palabra ingresada fue:${clavedescifrada}`;
+    //console.log(claveMayuscula + "\n"  + shiftInput)
+});
+
+
